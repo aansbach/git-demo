09136ed57476828ef924fe778d3c7e822744a70e
aansbach <79671939+aansbach@users.noreply.github.com>
2021-02-26 16:09:15 -0700

    Update GitDemo.java
    Commented my name, addNumber() function now adds the passed variable and previousNumber, added a new variable reverseNum, added an assignment statement to the reverseNumber() function.

--- GitDemo.java
+++ GitDemo.java
@@ -1,9 +1,10 @@
 /*****
- * Edit this line to have your name.
+ Andrew Ansbach
  ******/
 public class GitDemo {
     
     private double previousNumber;
+    private double reverseNum;
     
     public GitDemo() {
         previousNumber = 1;
@@ -13,8 +14,7 @@ public class GitDemo {
      * This function should add 'num' to 'previousNumber' and return the sum.
     *****/
     public double addNumber(double num) {
-        System.out.print("Add num to previousNumber");
-        return 0;
+        return num + previousNumber;
     }
     
     /*****
@@ -46,6 +46,7 @@ public class GitDemo {
             newNumber = newNumber * i;
         }
         
+        reverseNum = newNumber;
         return newNumber;
     }
     
